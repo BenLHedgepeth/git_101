ee4414c4fd9248968cc845724815f982b26abbdc
Ben Hedgepeth <airmaxmoney@protonmail.com>
2020-09-24 21:49:39 -0700

    added args, kwargs to Event class

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import datetime
 
 
 class Event:
-    def __init__(self, title):
+    def __init__(self, title, *args, **kwargs):
         self.title = title
         self.date = datetime.date.today()
+
+        for key, value in kwargs.items():
+            setattr(key, value)
 
     def __str__(self):
         return self.title
