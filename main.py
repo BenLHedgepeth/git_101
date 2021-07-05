858b7bfe8fb9372a40f99edfb81769b03d3d098f
Ben Hedgepeth <benhh@gmx.com>
2021-07-05 10:51:45 -0700

    Imported config dict

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 
 import datetime
+from config import connect_config
 
+# class Event:
+#     def __init__(self, title, *args, **kwargs):
+#         self.title = title
+#         self.date = datetime.date.today()
+#
+#         for key, value in kwargs.items():
+#             setattr(key, value)
+#
+#     def __str__(self):
+#         return self.title
 
-class Event:
-    def __init__(self, title, *args, **kwargs):
-        self.title = title
-        self.date = datetime.date.today()
-
-        for key, value in kwargs.items():
-            setattr(key, value)
-
-    def __str__(self):
-        return self.title
+if __name__ == "__main__":
+    print(connect_config)
